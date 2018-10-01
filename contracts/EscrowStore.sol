b9bd5057dfe236d0d3fd175269c8953c9fb98003
Sumit Chaudhari <sumit@cryptorai.com>
2018-10-01 16:21:35 +0800

    player info and betting variables added

--- contracts/EscrowStore.sol
+++ contracts/EscrowStore.sol
@@ -6,7 +6,28 @@ import "openzeppelin-solidity/contracts/payment/RefundEscrow.sol";
 import "openzeppelin-solidity/contracts/payment/SplitPayment.sol";
 
 contract EscrowStore is Escrow, PullPayment, RefundEscrow, SplitPayment {
-  constructor() {
-    
-  }
+	uint256 public minimumBet;
+	uint256 public totalBet;
+	uint256 public numberOfBets;
+	uint256 public maxAmountofBets = 100; 
+	address[] public players;
+
+	struct player {
+		uint256 amountBet;
+		string[] cardSelected;
+	}
+
+	mapping(adress => player) public playerInfo;
+
+	constructor(uint256 _minimumBet) PullPayment() {
+		if(_minimumBet != 0) minimumBet = _minimumBet;
+    }
+
+
+	mapping(address => uint256) public onBetMoney;
+
+	function makeBet(address _player, uint256 _bet) public payable {
+		onBetMoney[_player] = _bet;
+		Escrow.deposit(_bet);
+	}
 }
--- contracts/GetClue.sol
+++ contracts/GetClue.sol
@@ -7,9 +7,15 @@ contract GetClue is StandardToken, MintableToken {
     string public name;
     string public symbol;
     uint public decimals;
+
     constructor(string _name, string _symbol, uint _decimals) MintableToken() public {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
+        owner = msg.sender;
+    }
+
+    function kill() public {
+        if(msg.sender === owner) { selfdestruct(owner); }
     }
 }
